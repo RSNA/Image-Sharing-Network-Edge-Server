47b87186cbc7ea78f179d2ad1ab0e4e17ca5b933
smm <moores@mir.wustl.edu>
2011-03-04 17:14:56 -0600

    Add these functions so we can move more code out of the main body. This is just to make it easier to read. Also, added select_exam so we can test the exam table which is updated upon receipt of an HL7 message.  sub select_patient {  sub select_exam {  sub dump_hints

--- verif_valid/test-cases/A-3001/A-3001-01.pl
+++ verif_valid/test-cases/A-3001/A-3001-01.pl
@@ -5,6 +5,51 @@ use DBI;
 use lib "scripts";
 require image_sharing;
 
+sub select_patient {
+ my ($dbName, $testMRN) = @_;
+
+ @patient = image_sharing::select_patient_by_mrn($dbName, $testMRN);
+ if (scalar(@patient) < 5) {
+  $x = scalar(@patient);
+  if ($x == 0) {
+   print "No patient record returned for MRN: $testMRN\n";
+  } else {
+   print "Row for patient with MRN: $testMRN has only $x columns.\n";
+   print " We expected at least 5\n";
+  }
+  die "Failed A-3001-01";
+ }
+ return @patient;
+}
+
+sub select_exam {
+ my ($dbName, $pid) = @_;
+
+ @exam = image_sharing::select_exam_by_patient_id($dbName, $pid);
+ if (scalar(@exam) < 4) {
+  $x = scalar(@exam);
+  if ($x == 0) {
+   print "No Exam Record returned for PID: $pid\n";
+  } else {
+   print "Row for patient with PID: $pid has only $x columns.\n";
+   print " We expected at least 4\n";
+  }
+  die "Failed A-3001-01";
+ }
+ return @exam;
+}
+
+sub dump_hints
+{
+ my $x =
+ "The most likely cause of failure of this test is extracting the wrong\n" .
+ " Accession Number. This test assumes the Accession Number is a copy of\n" .
+ " the Filler Order Number, ORC-3.\n";
+
+ print $x;
+}
+
+
 ## Main starts here
  image_sharing::check_environment();
  image_sharing::clear_db("rsnadb");
@@ -25,14 +70,27 @@ require image_sharing;
   die "Could not send HL7 message to localhost" if $?;
  }
 
- @patient = image_sharing::select_patient_by_mrn("rsnadb", "A-3001-01");
+ $testMRN = "A-3001-01";
+ @patient = select_patient("rsnadb", $testMRN);
+ @exam    = select_exam("rsnadb", $patient[0]);
 
+ print "Start patient data test\n";
  my %patientHash;
  @localPatient = ("pid", "A-3001-01", "Clark^Wayne^", "1980-12-14", "M");
  %patientHash = image_sharing::append_patient_hash_global(@patient, %patientHash);
  %patientHash = image_sharing::append_patient_hash_local (@localPatient, %patientHash);
  $pass = image_sharing::check_patient(%patientHash);
 
- die "Failed A-3001-01" if ($pass == 0);
+ print "Start exam data test\n";
+ my %examHash;
+ @localExam = ("pid", "A-3001-01F", "Head MR", "modified-date");
+ %examHash = image_sharing::append_exam_hash_global(@exam, %examHash);
+ %examHash = image_sharing::append_exam_hash_local (@localExam, %examHash);
+ $pass += image_sharing::check_exam(%examHash);
+
+ if ($pass != 2) {
+  dump_hints();
+  die "Failed A-3001-01";
+ }
 
  print "A-3001-01 pass\n";
