476cac820e60b8629f67a267ae531b7669684f14
smm <moores@mir.wustl.edu>
2011-03-03 23:52:47 -0600

    A-3001-01.var and A-3001.pl are files that work together. The perl script (A-3001.pl) uses data (variables) from A-3001-01.var to construct HL7 messages used in test A-3001-01.

--- verif_valid/data/A-3001/A-3001.pl
+++ verif_valid/data/A-3001/A-3001.pl
@@ -3,18 +3,36 @@
 use Env;
 use File::Copy;
 use lib "..";
+use lib "../../scripts";
 require mesa_msgs;
+require xx;
 
+sub fill_variables {
+ my $path = shift;
+
+ open F, ">$path" or die "Could not open $f for append";
+
+ my $dt = image_sharing::generate_date_time();
+
+ print F "\$DATE_TIME\$ = $dt\n";
+ print F "\$ORDER_EFFECTIVE_DATE\$ = $dt\n";
+
+ close F;
+}
 
  # First, create all scheduling ORM messages
  @ files = ("A-3001-01");
  foreach $f(@files) {
   print "$f\n";
 
-  mesa_msgs::create_text_file(
-	"$f.var",		# This is the output
+  my $tmp = "/tmp/x.var";
+  fill_variables($tmp);
+  mesa_msgs::create_text_file_2_var_files(
+	"$f-orm.txt",		# This is the output
 	"../ihe_sched.tpl",	# Template for an O01 message
-	"$f-orm.txt");		# Input with order information
+	"$f.var",		# Input file 1
+	$tmp);			# Second input
+
   mesa_msgs::create_hl7("$f-orm");
   print "$f ORM created\n";
 
