0530a5355ae6c79cbc9fac870e55a60e10eef98a
Josh Stafford <jstafford1@umm.edu>
2012-03-29 13:18:15 -0400

    retrieve content installer fix

--- build/src/izpack/download-prep.sh
+++ build/src/izpack/download-prep.sh
@@ -2,7 +2,7 @@
 
 echo "executing download-prep.sh"
 
-cp -v $INSTALL_PATH/conf/truststore.jks $INSTALL_PATH/client_downloads/
+cp -v $INSTALL_PATH/conf/keystore.jks $INSTALL_PATH/client_downloads/
 
 echo "completing download-prep.sh"
 
