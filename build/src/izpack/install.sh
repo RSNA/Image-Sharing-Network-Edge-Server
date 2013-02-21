04ef036986860a8bd3c88286907016b02f668782
Wayne LaBelle <wlabelle@umm.edu>
2013-02-21 16:16:54 -0500

    Fixed CTP installer default working directory

--- build/src/izpack/install.sh
+++ build/src/izpack/install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+CTP_DIR=/usr/local/ctp
+INSTALL_SCRIPT=$(readlink -f $0)
+INSTALL_SCRIPT_DIR=$(cd ${INSTALL_SCRIPT%/*} && pwd)
+
 A=$(ps aux | grep [e]dgeserver | awk '{print $2}')
 
 if [ -n "$A" ]; then
   sudo service edge-server stop
 fi
 
-java -jar ${app.subpath}-${app.version}-standard.jar
+java -jar "$INSTALL_SCRIPT_DIR/${app.subpath}-${app.version}-standard.jar"
+
+mkdir -p $CTP_DIR &&
+cp -v $INSTALL_SCRIPT_DIR/ISN-installer.jar $CTP_DIR &&
+cd $CTP_DIR &&
 java -jar ISN-installer.jar
 
 sudo service edge-server start
