7b7c834a5d90765e2553ac12ec4de76b059ae8f9
Steve Moore <moores@mir.wustl.edu>
2013-08-08 15:51:21 -0500

    These scripts create the test data for tests in the A-2001 series. I have also included the two scripts thar run test A-2001-03.

---
+++ verif_valid/test-cases/A-2001/A-2001-03a.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use Env;
+use DBI;
+use lib "scripts";
+require image_sharing;
+
+## Main starts here
+ image_sharing::check_environment();
+ image_sharing::clear_db("rsnadb");
+
+ my $verbose = "";
+ $verbose = "1" if (scalar(@ARGV) > 0);
+
+ $schemaVersion= image_sharing::select_schema_version("rsnadb");
+ print "$schemaVersion\n";
+ if (!($schemaVersion eq "3.1.0")) {
+  print "Test A-2001-03(a) fails.\n";
+  print "Schema version is expected to be 3.1.0, not $schemaVersion.\n";
+  exit(1);
+ }
+
