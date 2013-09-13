bfb6ba41f04c1ff6f88846d53cfd33af777967dd
Steve Moore <moores@mir.wustl.edu>
2013-09-13 16:21:21 -0500

    Modified A-5006/A-5006 to complete the set of 4 test cases we need.

--- verif_valid/data/A-5006/A-5006.pl
+++ verif_valid/data/A-5006/A-5006.pl
@@ -8,14 +8,18 @@ require mesa_msgs;
 require image_sharing;
 
 sub fill_variables{
- my ($path, $delta) = @_;
+ my ($path, $delta, $orderStatus) = @_;
 
  open F, ">$path" or die "Could not open $f for append";
 
  my $dt = image_sharing::generate_date_time_with_delta($delta);
 
+ print F "\$ADMIT_DATE_TIME\$ = $dt\n";
  print F "\$DATE_TIME\$ = $dt\n";
  print F "\$ORDER_EFFECTIVE_DATE\$ = $dt\n";
+ print F "\$UPDATE_DATE_TIME\$ = $dt\n";
+
+ print F "\$ORDER_STATUS\$ = $orderStatus\n";
 
  close F;
 }
@@ -33,7 +37,7 @@ sub fill_variables{
   print "$f\n" if ($verbose);
 
   my $tmp = "/tmp/x.var";
-  fill_variables($tmp, 1, "SC");
+  fill_variables($tmp, 2, "SC");
   mesa_msgs::create_text_file_2_var_files(
 	"$f-orm.txt",		# This is the output
 	"../ihe_sched.tpl",	# Template for an O01 message
@@ -43,24 +47,26 @@ sub fill_variables{
   mesa_msgs::create_hl7("$f-orm");
   print "Created: $f-orm\n" if ($verbose);
 
-  fill_variables($tmp, 0, "C");
-  mesa_msgs::create_text_file(
-	"$f.var",		# This is the output
-	"../ihe_oru.tpl",	# Template for an O01 message
-	"$f-oru.txt");		# Input with order information
-  mesa_msgs::create_hl7("$f-oru");
-  print "Created: $f-oru\n" if ($verbose);
-
-  $x = "cat $f.var complete.var > $f-complete.var";
-  `$x`;
+#  $x = "cat $f.var complete.var > $f-complete.var";
+#  `$x`;
+  fill_variables($tmp, 1, "CM");
   mesa_msgs::create_text_file_2_var_files(
-	"$f-complete.txt",		# This is the output
+	"$f-complete.txt",	# This is the output
 	"../ihe_sched.tpl",	# Template for an O01 message
-	"$f-complete.var",		# Input file 1
+	"$f.var",		# Input file 1
 	$tmp);			# Second input
-
   mesa_msgs::create_hl7("$f-complete");
   print "Created: $f-complete\n" if ($verbose);
-  unlink ("$f-complete.var");
+
+#  unlink ("$f-complete.var");
+
+  fill_variables($tmp, 0, "F");
+  mesa_msgs::create_text_file_2_var_files(
+	"$f-oru.txt",		# This is the output
+	"../ihe_oru.tpl",	# Template for an O01 message
+	"$f.var",		# Input file 1
+	$tmp);			# Second input
+  mesa_msgs::create_hl7("$f-oru");
+  print "Created: $f-oru\n" if ($verbose);
 
  }
