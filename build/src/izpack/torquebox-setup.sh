36bf632043fb2e681688c38591996df95658ad89
Wayne LaBelle <wlabelle@umm.edu>
2014-10-24 15:51:03 -0400

    Make the torquebox setup script smarter and more verbose

--- build/src/izpack/torquebox-setup.sh
+++ build/src/izpack/torquebox-setup.sh
@@ -27,34 +27,56 @@ chown -R edge:edge $INSTALL_PATH &&
 chmod +x $JBOSS_CLI &&
 chmod +x $TORQUEBOX_HOME/jboss/bin/standalone.sh &&
 
+edge_start_wait() {
+    for i in {1..10}
+    do
+	if netstat -an46 | grep -q :9990 ; then
+	    echo
+	    echo "edge-server started."
+	    break
+	fi
+	sleep 5
+	echo -n "."
+    done
+}
+
+wait_for_file() {
+    for i in {1..10}
+    do
+	if [ -f "$1" ] ; then
+	    break
+	fi
+	sleep 5
+    done
+}
+
+
 start edge-server
 echo
 echo -n "waiting for edge-server to start."
-for i in {1..10}
-do
-    if netstat -an46 | grep -q :9990 ; then
-	echo
-	echo "edge-server started."
-	break
-    fi
-    sleep 5
-    echo -n "."
-done
+edge_start_wait
 
 if ! $JBOSS_CLI -c "ls /subsystem=datasources" 2>&1 > /dev/null ; then
+    echo "Adding datasources subsystem..."
     $JBOSS_CLI -c "/subsystem=datasources:add"
-    $JBOSS_CLI -c "reload" 
+    echo -n "Restarting edge-server."
+    restart edge-server
     sleep 5
+    edge_start_wait
 fi
 
+echo "Adding OpenAM Overlays"
 $JBOSS_CLI -c "deployment-overlay add --name=OpenAMOverlay --content=/WEB-INF/classes/bootstrap.properties=$INSTALL_PATH/scripts/bootstrap.properties\,/WEB-INF/jboss-deployment-structure.xml=$INSTALL_PATH/scripts/jboss-deployment-structure.xml --deployments=openam.war"
 
 $JBOSS_CLI -c "deployment-overlay add --name=AgentOverlay --content=/WEB-INF/jboss-deployment-structure.xml=$INSTALL_PATH/scripts/jboss-deploy-agent.xml --deployments=agentapp.war"
 
 # FIXME if upgrade mod openam.war with token auth
 
+echo "Deploying OpenAM war"
 cp -v $INSTALL_PATH/openam-server-%{openam.version}.war $TORQUEBOX_HOME/jboss/standalone/deployments/openam.war &&
-sleep 5 &&
+wait_for_file $TORQUEBOX_HOME/jboss/standalone/deployments/openam.war.deployed
+
+echo "Running OpenAM configurator"
 $JAVA_HOME/bin/java -jar $INSTALL_PATH/openam-configurator-tool-%{openam.version}.jar -f $INSTALL_PATH/scripts/openam.config 
 echo %{openam.admin_pwd} > $INSTALL_PATH/conf/ampwd.txt
 chmod 400 $INSTALL_PATH/conf/ampwd.txt
@@ -65,6 +87,7 @@ echo $AGENT_PWD > $INSTALL_PATH/conf/amagentpwd.txt
 chmod 400 $INSTALL_PATH/conf/amagentpwd.txt
 chown edge $INSTALL_PATH/conf/amagentpwd.txt 
 
+echo "Configuring ssoadm tool."
 mkdir -p $INSTALL_PATH/ssoadm/openam
 SSOADM=$INSTALL_PATH/ssoadm/openam/ssoadm
 
@@ -85,20 +108,27 @@ chmod +x $SSOADM
 $SSOADM update-server-cfg -u amAdmin -f %{rsna.root}/conf/ampwd.txt -s default -a com.iplanet.am.cookie.name=RSNA_SSO
 $SSOADM create-agent -u amAdmin -f %{rsna.root}/conf/ampwd.txt -e / -t J2EEAgent -b TorqueBoxAgent -s %{server.host}:3000/openam -g http://%{server.host}:3000/agentapp -v -a userpassword=$AGENT_PWD com.iplanet.am.cookie.name=RSNA_SSO
 
-
+echo "Adding Postgres Module and DataSource"
 $JBOSS_CLI -c "module add --name=org.postgres --resources=$POSTGRES_JAR --dependencies=javax.api\,javax.transaction.api"
 $JBOSS_CLI -c '/subsystem=ee:write-attribute(name="global-modules",value=[{"name"=>"org.postgres","slot"=>"main"}])'
 $JBOSS_CLI -c '/subsystem=datasources/jdbc-driver=postgres:add(driver-name="postgres",driver-module-name="org.postgres",driver-class-name=org.postgresql.Driver)'
 $JBOSS_CLI -c "data-source add --jndi-name=java:/rsnadbDS --name=rsnadbPool --connection-url=jdbc:postgresql://$DBHOST:$DBPORT/rsnadb --driver-name=postgres --user-name=$DBUSER --password=$DBPASS"
 
+echo "Installing OpenAM Agent."
 AGENT_MOD=$INSTALL_PATH/j2ee_agents/jboss_v7_agent/config/module.xml
 mv $AGENT_MOD $AGENT_MOD.old
 cp -v $INSTALL_PATH/scripts/agent-module.xml $AGENT_MOD
 $INSTALL_PATH/j2ee_agents/jboss_v42_agent/bin/agentadmin --install --acceptLicense --useResponse $INSTALL_PATH/scripts/agent-res.txt
 
+restart edge-server
+sleep 5
+edge_start_wait
+
+echo "Deploying TokenApp"
 $JBOSS_CLI -c "deploy $INSTALL_PATH/token-app.knob"
 
-sleep 5
+wait_for_file $TORQUEBOX_HOME/jboss/standalone/deployments/token-app.knob.deployed
+
 stop edge-server
 
 echo "completing torquebox-setup.sh"
