c65bf7883b5f10917a9dd0bf5060c22c68753e25
Wayne LaBelle <wlabelle@umm.edu>
2014-11-07 12:39:06 -0500

    Remove incorrect message in db setup script

--- build/src/izpack/edge-db-install.sh
+++ build/src/izpack/edge-db-install.sh
@@ -30,7 +30,6 @@ if [ "x$UPGRADE" != 'x1' ]; then
   echo "creating edge user"
   /usr/bin/env psql -v ON_ERROR_STOP=1 --pset pager=off -X -q -w -h $DBHOST -p $DBPORT -U $SUPERUSER postgres -c "CREATE ROLE edge WITH NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN ENCRYPTED PASSWORD '$DBPASSWD'" &&
   /usr/bin/env psql -v ON_ERROR_STOP=1 --pset pager=off -X -q -w -h $DBHOST -p $DBPORT -U $SUPERUSER postgres < $SQLFILE || exit 1
-  echo "creating admin user to web interface, default password is 'password'"
   /usr/bin/env psql -v ON_ERROR_STOP=1 --pset pager=off -X -q -w -h $DBHOST -p $DBPORT -U $SUPERUSER rsnadb <<EOF
 INSERT INTO configurations (key, value, modified_date) values ('tmp-dir-path','$TMPPATH', now());
 EOF
