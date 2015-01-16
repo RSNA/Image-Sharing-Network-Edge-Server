3d56e7c99a71a70543c14b2d5efd1d4013e79d52
Wayne LaBelle <wlabelle@umm.edu>
2015-01-16 10:40:37 -0500

    Use command batching for OpenAM ssoadm call

--- build/src/izpack/torquebox-setup.sh
+++ build/src/izpack/torquebox-setup.sh
@@ -102,18 +102,22 @@ sed -e "$(sed_e 'JAVA_HOME' "$JAVA_HOME")" \
 
 chmod +x $SSOADM
 
-$SSOADM update-server-cfg -u amAdmin -f %{rsna.root}/conf/ampwd.txt -s default -a com.iplanet.am.cookie.name=RSNA_SSO
+# Batch $SSOADM calls
+cat <<EOF > /tmp/ssoadm.batch
+update-server-cfg -s default -a com.iplanet.am.cookie.name=RSNA_SSO
+create-identity -e / -t Group -i Admin
+create-identity -e / -t Group -i Super
+create-identity -e / -t Group -i Export
+create-identity -e / -t Group -i Import
+create-identity -e / -t User -i admin -a givenname=Admin sn=Admin userpassword=password
+add-member -e / -t Group -i Admin -m admin -y User
+delete-identities -e / -t User -i demo
+delete-identities -e / -t User -i anonymous
+EOF
 
 echo "Creating admin user and Groups"
-$SSOADM create-identity -u amAdmin -f %{rsna.root}/conf/ampwd.txt -e / -t Group -i Admin
-$SSOADM create-identity -u amAdmin -f %{rsna.root}/conf/ampwd.txt -e / -t Group -i Super
-$SSOADM create-identity -u amAdmin -f %{rsna.root}/conf/ampwd.txt -e / -t Group -i Export
-$SSOADM create-identity -u amAdmin -f %{rsna.root}/conf/ampwd.txt -e / -t Group -i Import
-$SSOADM create-identity -u amAdmin -f %{rsna.root}/conf/ampwd.txt -e / -t User -i admin -a givenname=Admin sn=Admin userpassword=password
-$SSOADM add-member -u amAdmin -f %{rsna.root}/conf/ampwd.txt -e / -t Group -i Admin -m admin -y User
-# cleanup default users
-$SSOADM delete-identities -u amAdmin -f %{rsna.root}/conf/ampwd.txt -e / -t User -i demo
-$SSOADM delete-identities -u amAdmin -f %{rsna.root}/conf/ampwd.txt -e / -t User -i anonymous
+
+$SSOADM do-batch -u amAdmin -f %{rsna.root}/conf/ampwd.txt -c -Z /tmp/ssoadm.batch
 
 echo "Adding Postgres Module and DataSource"
 $JBOSS_CLI -c "module add --name=org.postgres --resources=$POSTGRES_JAR --dependencies=javax.api\,javax.transaction.api"
