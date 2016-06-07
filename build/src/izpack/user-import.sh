c21a3afcd27a8d389a0d496848be156678e4c4eb
Wayne LaBelle <wlabelle@umm.edu>
2016-06-07 12:41:14 -0400

    Update user export and add import script

---
+++ build/src/izpack/user-import.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+. /etc/rsna.conf
+
+TMP=$(mktemp --suffix=ldif)
+
+sed 's/o=openam/dc=isn,dc=rsna,dc=org/g' < $1 > $TMP
+chmod +r $TMP
+
+$RSNA_ROOT/opendj/bin/import-ldif -h localhost -t 0 -n userRoot -a -D "cn=Directory Manager" --port 4444 -w Hazmiljad4 -l $TMP -X
