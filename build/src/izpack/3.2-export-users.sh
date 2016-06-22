6e76b87c77e89fe025e889d8338dcbe22796bde7
Wayne LaBelle <wlabelle@umm.edu>
2016-06-22 13:57:45 -0400

    Change ldif output path in 3.2 user export script

--- build/src/izpack/3.2-export-users.sh
+++ build/src/izpack/3.2-export-users.sh
@@ -2,4 +2,4 @@
 
 . /etc/rsna.conf
 
-$RSNA_ROOT/openam-cfg/opends/bin/export-ldif --port 4444 --hostname 127.0.0.1 --bindDN "cn=Directory Manager" --bindPassword chang3me --includeBranch ou=people,o=openam --includeBranch ou=groups,o=openam --backendID userRoot --ldifFile ~/user-export.ldif --start 0 --trustAll
+$RSNA_ROOT/openam-cfg/opends/bin/export-ldif --port 4444 --hostname 127.0.0.1 --bindDN "cn=Directory Manager" --bindPassword chang3me --includeBranch ou=people,o=openam --includeBranch ou=groups,o=openam --backendID userRoot --ldifFile /tmp/user-export.ldif --start 0 --trustAll
