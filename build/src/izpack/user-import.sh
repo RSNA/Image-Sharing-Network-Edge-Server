7ad678c5d6b823e146f8777d86cd53b3d09fa4bb
Wayne LaBelle <wlabelle@umm.edu>
2016-06-22 13:56:29 -0400

    Add replace option to import-ldif call in user import script

--- build/src/izpack/user-import.sh
+++ build/src/izpack/user-import.sh
@@ -7,4 +7,4 @@ TMP=$(mktemp --suffix=ldif)
 sed 's/o=openam/dc=isn,dc=rsna,dc=org/g' < $1 > $TMP
 chmod +r $TMP
 
-$RSNA_ROOT/opendj/bin/import-ldif -h localhost -t 0 -n userRoot -a -D "cn=Directory Manager" --port 4444 -w Hazmiljad4 -l $TMP -X
+$RSNA_ROOT/opendj/bin/import-ldif -h localhost -t 0 -n userRoot -a -r -D "cn=Directory Manager" --port 4444 -w Hazmiljad4 -l $TMP -X
